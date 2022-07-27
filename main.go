df6b70f77cca31b82ba44300f801c70d0e0b3ab4
digantatessell <diganta.paladhi@tessell.io>
2022-07-27 12:48:56 +0530

    initial commit v0.0.2

--- main.go
+++ main.go
@@ -18,6 +18,7 @@ import (
 
 // Run the docs generation tool, check its repository for more information on how it works and how docs
 // can be customized.
+// Generate the Terraform provider documentation using `tfplugindocs`:
 //go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs
 
 var (
